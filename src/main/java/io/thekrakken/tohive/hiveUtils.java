18d518a375f009a1489ccc6a73805cdbf260aabc
anthony corbacho <corbacho.anthony@gmail.com>
2014-04-25 23:37:02 +0900

    reindentation

--- src/main/java/io/thekrakken/tohive/hiveUtils.java
+++ src/main/java/io/thekrakken/tohive/hiveUtils.java
@@ -37,10 +37,12 @@ public class hiveUtils {
   private static Integer COMMA = 0;
 
   public static final Pattern DATE_PATTERN = Pattern
-      .compile("^[\"]?([0-9]{4}[-/][0-9]{2}[-/][0-9]{2})[T ]" + "([0-9]{2}:[0-9]{2}:[0-9]{2})"
-          + "([ ]?([-+][0-9]{2}[:]?[0-9]{2})|Z)[\"]?$");
+      .compile("^[\"]?([0-9]{4}[-/][0-9]{2}[-/][0-9]{2})[T ]"+
+              "([0-9]{2}:[0-9]{2}:[0-9]{2})"+
+              "([ ]?([-+][0-9]{2}[:]?[0-9]{2})|Z)[\"]?$");
 
   public static final Pattern HEX_PATTERN = Pattern.compile("^([0-9a-fA-F][0-9a-fA-F])*$");
+
   // primitive
   public final static String STRING = "STRING";
   public final static String TINYINT = "TINYINT";
