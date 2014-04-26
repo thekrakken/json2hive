3b5c0d2ea3933e53dd83eed1bb299c35c1db46ab
anthony corbacho <corbacho.anthony@gmail.com>
2014-04-26 10:57:46 +0900

    rename package

--- src/main/java/io/thekrakken/json2hive/Json.java
+++ src/main/java/io/thekrakken/json2hive/Json.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *******************************************************************************/
-package io.thekrakken.tohive;
+package io.thekrakken.json2hive;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
